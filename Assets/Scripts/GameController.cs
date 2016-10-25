d6183a414a14a876eeb21f4e03924f4e3f34078d
tristan <poke_digi@hotmail.fr>
2016-10-25 15:13:26 +0200

    BUG Fixe : The cheat squares are now correctly disabled

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -71,15 +71,43 @@ public class GameController : MonoBehaviour {
 	{
 		Vector2 position = GetCurrentPlayer().playerPosition;
 
+		//We check from x = -diceValue to x = diceValue
 		for (int i = -diceValue; i < (diceValue + 1); i++)
 		{
+			//We browse all the squares to see if one has a matching position
 			for (int j = 0; j < buttonList.Length; j++)
 			{
 				Vector2 squarePosition = buttonList[j].GetComponent<Squares>().GetSquarePosition();
-				int xOffset = diceValue - Mathf.Abs(i);
-				if (squarePosition == new Vector2(position.x - xOffset, position.y + i) || squarePosition == new Vector2(position.x + xOffset, position.y + i))
+				/* this offset is here to check squares that are not on the same y axis
+				 * Exemple : if the player position is (4, 4) and the dice rolls 3
+				 * x = -3, so offset = 0 (3 - abs(-3)), so we check is a square is at [1, 4] ((4 - 3, 4 - offset) == (4 - 3, 4 + offset))
+				 * x = -2, so offset = 1 (3 - abs(-2)), so we check is a square is either at [2, 3] (4 - 2, 4 - offset) or [2, 5] (4 - 2, 4 + offset)
+				 */
+				int yOffset = diceValue - Mathf.Abs(i);
+				if (squarePosition == new Vector2(position.x + i, position.y - yOffset) || squarePosition == new Vector2(position.x + i, position.y + yOffset))
 				{
-					buttonList[j].GetComponent<Button>().interactable = true;
+					//if the dice value equal 4 we have to remove the squares that can be accessed by using non existing squares
+					if (diceValue == 4)
+					{
+						// If the square is on the same x axis
+						if (i == -4 || i == 4)
+						{
+							// if there is a square on the right AND the left of the player the square is validated, otherwise it's a cheat square
+							if (IsASquareAtPos(new Vector2(position.x - 1, position.y)) && IsASquareAtPos(new Vector2(position.x + 1, position.y)))
+								buttonList[j].GetComponent<Button>().interactable = true;
+						}
+						// If the square is on the same y axis
+						else if (i == 0)
+						{
+							// if there is a square on the top AND the bottom of the player the square is validated, otherwise it's a cheat square
+							if (IsASquareAtPos(new Vector2(position.x, position.y - 1)) && IsASquareAtPos(new Vector2(position.x, position.y + 1)))
+								buttonList[j].GetComponent<Button>().interactable = true;
+						}
+						else
+							buttonList[j].GetComponent<Button>().interactable = true;
+					}
+					else
+						buttonList[j].GetComponent<Button>().interactable = true;
 				}
 			}
 		}
@@ -95,6 +123,18 @@ public class GameController : MonoBehaviour {
 		{
 			buttonList[i].GetComponent<Button>().interactable = toggle;
 		}
+	}
+
+	bool IsASquareAtPos(Vector2 pos)
+	{
+		for (int i = 0; i < buttonList.Length; i++)
+		{
+			if (buttonList[i].GetComponent<Squares>().GetSquarePosition() == pos)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/*
